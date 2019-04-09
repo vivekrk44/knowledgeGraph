92d7ec6a9b1198cbf382712b7ddce9285e0f974b
vivekalgorythma <vivek.radhakrishnan@algorythma.com>
2019-04-09 21:00:22 +0400

    removed un necessary things

--- Assets/_scripts/data_reader.cs
+++ Assets/_scripts/data_reader.cs
@@ -2,20 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class teamData
-{
-    public string category;
-    public int teamNo;
-    public string teamName;
-    public string feasablilityScore;
-    public string somethingA;
-    public string somethingB;
-    public string thisA;
-    public string thisB;
-    public string thatA;
-    public string thatB;
-};
-
 public class data_reader : MonoBehaviour
 {
     public List <teamData> dat;
@@ -23,7 +9,6 @@ public class data_reader : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        getData();
     }
 
     // Update is called once per frame
@@ -32,25 +17,5 @@ public class data_reader : MonoBehaviour
         
     }
 
-    void getData()
-    {
-        for (int i=0; i<10; i++)
-        {
-            teamData tmp = new teamData();
-            tmp.teamNo = i;
-            tmp.teamName = i.ToString();
-            tmp.feasablilityScore = (i + 4).ToString();
 
-            tmp.somethingA = "somethingA: " + i.ToString();
-            tmp.somethingB = "somethingB: " + i.ToString();
-
-            tmp.thatA = "thatA: " + i.ToString();
-            tmp.thatB = "thatB: " + i.ToString();
-
-            tmp.thisA = "thisA: " + i.ToString();
-            tmp.thisB = "thisB: " + i.ToString();
-
-            dat.Add(tmp);
-        }
-    }
 }

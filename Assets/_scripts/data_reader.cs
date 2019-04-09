995d202c578fd3066cd421f85b947d0265fab130
vivekalgorythma <vivek.radhakrishnan@algorythma.com>
2019-04-09 20:49:44 +0400

    initial commit

---
+++ Assets/_scripts/data_reader.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teamData
+{
+    public string category;
+    public int teamNo;
+    public string teamName;
+    public string feasablilityScore;
+    public string somethingA;
+    public string somethingB;
+    public string thisA;
+    public string thisB;
+    public string thatA;
+    public string thatB;
+};
+
+public class data_reader : MonoBehaviour
+{
+    public List <teamData> dat;
+    public int state = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        getData();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void getData()
+    {
+        for (int i=0; i<10; i++)
+        {
+            teamData tmp = new teamData();
+            tmp.teamNo = i;
+            tmp.teamName = i.ToString();
+            tmp.feasablilityScore = (i + 4).ToString();
+
+            tmp.somethingA = "somethingA: " + i.ToString();
+            tmp.somethingB = "somethingB: " + i.ToString();
+
+            tmp.thatA = "thatA: " + i.ToString();
+            tmp.thatB = "thatB: " + i.ToString();
+
+            tmp.thisA = "thisA: " + i.ToString();
+            tmp.thisB = "thisB: " + i.ToString();
+
+            dat.Add(tmp);
+        }
+    }
+}
---
+++ Assets/_scripts/state_machine.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class state_machine : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    public Canvas node0;
+    public Canvas node1;
+    public Canvas node2;
+    public Canvas node3;
+    public Canvas node4;
+    public int state = 0;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(state)
+        {
+            case 0:
+                case0UI();
+                break;
+
+            case 1:
+                case1UI();
+                break;
+
+            default:
+                Debug.Log("Wrong state");
+                break;
+        }
+    }
+
+    public void node1_pressed()
+    {
+        state = 1;
+    }
+
+    void case0UI()
+    {
+        node0.enabled = true;
+        node1.enabled = false;
+        node2.enabled = false;
+        node3.enabled = false;
+        node4.enabled = false;
+        return;
+    }
+
+    void case1UI()
+    {
+        node0.enabled = false;
+        node1.enabled = true;
+        node2.enabled = false;
+        node3.enabled = false;
+        node4.enabled = false;
+        return;
+    }
+}
